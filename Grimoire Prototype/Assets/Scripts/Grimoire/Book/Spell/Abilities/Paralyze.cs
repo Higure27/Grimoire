3710ec7f14569cffd3d9623ee94f4ba861ffd704
Higure27 <kevan.or@gmail.com>
2015-09-27 13:58:12 -0600

    Paralyze Script

---
+++ Grimoire Prototype/Assets/Scripts/Grimoire/Book/Spell/Abilities/Paralyze.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paralyze : BaseAbility 
+{
+	public void DoAbility(BasePlayerSummon p1, BasePlayerSummon p2)
+	{
+		p2.Paralyze = Random.Range(2, 4);
+	}
+}
