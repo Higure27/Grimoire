2c9df85b8e5eabb5db283d0f530a9284d90e9e49
Matt Lemon <u0575787@utah.edu>
2015-10-20 16:26:06 -0600

    updating site

--- Grimoire Prototype/Assets/Scripts/Grimoire/Database/GrimoireDatabase.cs
+++ Grimoire Prototype/Assets/Scripts/Grimoire/Database/GrimoireDatabase.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GrimoireDatabase : MonoBehaviour {
+public class GrimoireDatabase
+{
+    // Load Player Function
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Save Player Function
+
 }
