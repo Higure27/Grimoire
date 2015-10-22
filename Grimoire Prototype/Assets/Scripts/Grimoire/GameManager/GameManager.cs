266e811a591006aa19986819838c2ec54940ca28
Matt Lemon <u0575787@utah.edu>
2015-10-22 12:52:02 -0600

    Stupid Ignore File

---
+++ Grimoire Prototype/Assets/Scripts/Grimoire/GameManager/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameStates
+    {
+        START,
+        MAIN,
+        BATTLE,
+        BOOK
+    }
+
+    public static GameManager instance = null;
+    public GameStates current_state = GameStates.START;
+    public BasePlayer player;
+
+    // Makes sure each scene only loads once
+    public bool scene_loaded = false;
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    switch (current_state)
+        {
+            case GameStates.START:
+                if (!scene_loaded)
+                {
+                    Application.LoadLevel("start_scene");
+                    scene_loaded = true;
+                }
+                break;
+            case GameStates.MAIN:
+                break;
+            case GameStates.BOOK:
+                break;
+            case GameStates.BATTLE:
+                if(!scene_loaded)
+                {
+                    Application.LoadLevel("battle_scene");
+                    scene_loaded = true;
+                }
+                break;
+        }
+	}
+}
---
+++ Grimoire Prototype/Assets/Scripts/Grimoire/GameManager/GameStates/StartScreen.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using UnityEngine.UI;
+using System.IO;
+
+public class StartScreen : MonoBehaviour
+{
+    public GameObject user_name;
+
+    private XmlDocument spell_book_db = new XmlDocument();
+    private XmlNode spell_book_root;
+    //private XmlNamespaceManager spell_book_nsmgr;
+    //private XmlTextReader spell_book_reader;
+
+
+    void Awake()
+    {
+        //spell_book_db.Load("SpellBookDatabase.xml");
+        //spell_book_root = spell_book_db.DocumentElement;
+        //spell_book_nsmgr = new XmlNamespaceManager(spell_book_db.NameTable);
+        //spell_book_nsmgr.AddNamespace("sbk", "urn:spellbooks-schema");
+        //spell_book_reader = new XmlTextReader("SpellBookDatabase.xml");
+    }
+
+    public void Game_Start()
+    {
+        GameManager.instance.current_state = GameManager.GameStates.BATTLE;
+        GameManager.instance.scene_loaded = false;
+    }
+
+    public void Login()
+    {
+        string user = user_name.GetComponentsInChildren<Text>()[1].text;
+        if(user == null || user == "")
+        {
+            return;
+        }
+        spell_book_db.Load("Assets/Scripts/Grimoire/Database/SpellBookDatabase.xml");
+        spell_book_root = spell_book_db.DocumentElement;
+        string path = "/Book[@id='" + user + "']";
+        Debug.Log(path);
+        XmlNode book = spell_book_root.SelectSingleNode(path);
+        Debug.Log("GOT HERE");
+        if (book == null)
+        {
+            return;
+        }
+        // Setup Player
+        GameManager.instance.player = new BasePlayer();
+        GameManager.instance.player.Player_Name = user;
+        GameManager.instance.player.Player_Spell_Book = new SpellBook();
+        GameManager.instance.player.Players_Summon = new BasePlayerSummon();
+        // Setup Players Summon
+        GameManager.instance.player.Players_Summon.Summon_Name = "Paladin";
+        GameManager.instance.player.Players_Summon.Summon_Level = 1;
+        GameManager.instance.player.Players_Summon.Summon_Class = new BaseLightSummon();
+        GameManager.instance.player.Players_Summon.Strength = GameManager.instance.player.Players_Summon.Summon_Class.Strength;
+        GameManager.instance.player.Players_Summon.Defense = GameManager.instance.player.Players_Summon.Summon_Class.Defense;
+        GameManager.instance.player.Players_Summon.Health = GameManager.instance.player.Players_Summon.Summon_Class.Health;
+        GameManager.instance.player.Players_Summon.Burn = 0;
+        GameManager.instance.player.Players_Summon.Poison = 0;
+        GameManager.instance.player.Players_Summon.Paralyze = 0;
+        GameManager.instance.player.Players_Summon.Attack_Boost = false;
+        GameManager.instance.player.Players_Summon.Defense_Boost = false;
+
+        foreach(XmlNode spell in book.ChildNodes)
+        {
+            Add_Spell(spell.InnerText);
+        }
+
+        Game_Start();
+    }
+
+    private void Add_Spell(string spell)
+    {
+        switch (spell)
+        {
+            // Attack Spells
+            case "BurnAttack":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new BurnAttack());
+                break;
+            case "DragonBite":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new DragonBite());
+                break;
+            case "ParalyzingGrasp":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new ParalyzingGrasp());
+                break;
+            case "PoisonAttack":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new PoisonAttack());
+                break;
+            case "Shriek":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new Shriek());
+                break;
+            case "VampireSlash":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new VampireSlash());
+                break;
+            // Defense Spells
+            case "BurningShield":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new BurningShield());
+                break;
+            case "Cure":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new CureSelf());
+                break;
+            case "GlaringShield":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new GlaringShield());
+                break;
+            case "Heal":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new HealSelf());
+                break;
+            case "PoisonNeedle":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new PoisonNeedle());
+                break;
+            case "VampiricShield":
+                GameManager.instance.player.Player_Spell_Book.AddSpell(new VampiricShield());
+                break;
+        }
+    }
+}
